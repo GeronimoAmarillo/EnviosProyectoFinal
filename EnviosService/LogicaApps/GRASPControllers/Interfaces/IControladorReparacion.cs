8fc3eeca9606bf21ab7922760b5fda5a1105e882
Gero Amarillo <geronimoamarillo29@gmail.com>
2018-05-01 17:46:32 -0300

    Agregada la logica de las apps, las cuales van a trabajar simplemente con la memoria del sistema y se comunicaran con el servicio expuesto

--- EnviosService/LogicaApps/GRASPControllers/Interfaces/IControladorReparacion.cs
+++ EnviosService/LogicaApps/GRASPControllers/Interfaces/IControladorReparacion.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logica
+namespace Logica.GRASPControllers.Interfaces
 {
-    class FabricaLogica
+    class IControladorReparacion
     {
     }
 }
