3265670e5b68303a9c033a83748294e654e78664
Pablo <pablorcarmona@hotmail.com>
2018-05-19 17:27:16 -0300

    avances alta cliente en app

--- EnviosService/AdministradoresApp/Controllers/UsuariosController.cs
+++ EnviosService/AdministradoresApp/Controllers/UsuariosController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using AdministradoresApp.Models;
+using EntidadesCompartidas;
+using AutoMapper;
 
 namespace EnviosService.Controllers
 {
@@ -11,5 +14,36 @@ namespace EnviosService.Controllers
     [Route("api/Usuarios")]
     public class UsuariosController : Controller
     {
+        public UsuariosController()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Usuarios, DTUsuario>()
+                    .ForMember(d => d.Id, opt => opt.MapFrom(u => u.Id))
+                    .ForMember(d => d.NombreUsuario, opt => opt.MapFrom(u => u.NombreUsuario))
+                    .ForMember(d => d.Contraseña, opt => opt.MapFrom(u => u.Contraseña))
+                    .ForMember(d => d.Nombre, opt => opt.MapFrom(u => u.Nombre))
+                    .ForMember(d => d.Telefono, opt => opt.MapFrom(u => u.Telefono))
+                    .ForMember(d => d.Direccion, opt => opt.MapFrom(u => u.Direccion))
+                    .ForMember(d => d.Email, opt => opt.MapFrom(u => u.Email))
+                ;
+            });
+        }
+
+        public ActionResult CrearUsuarios()
+        {
+            return View();
+        }
+
+        public ActionResult Crear(DTUsuario unUsuario)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuarios usuarioXAgregar = Mapper.Map<Usuarios>(unUsuario);
+                //llamar a servicio
+                return View("Index");
+            }
+            return View("Error");
+        }
     }
 }
