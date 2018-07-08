1aa7ca7b410d8a2f5de6da6f22f517e3d6861234
Gero Amarillo <geronimoamarillo29@gmail.com>
2018-07-08 16:03:19 -0300

    Todo codificado pero sin testear

--- EmpleadosApp/EmpleadosApp.Android/AltaPalletActivity.cs
+++ EmpleadosApp/EmpleadosApp.Android/AltaPalletActivity.cs
@@ -22,6 +22,7 @@ namespace EmpleadosApp.Droid
         private ListView lvClientes;
         private Cliente clienteSeleccionado;
         private int idRack;
+        private int rutCliente;
         private Button btnAltaCliente;
         private EditText etProducto;
         private EditText etCantidad;
@@ -48,7 +49,7 @@ namespace EmpleadosApp.Droid
                     }
                     catch (Exception ex)
                     {
-                        throw new Exception("Se produjo un error al intentar listar los racks.");
+                        throw new Exception("Se produjo un error al intentar listar los clientes.");
                     }
                 }
 
@@ -64,9 +65,25 @@ namespace EmpleadosApp.Droid
             }
         }
 
-        private void ListarClientes()
+        private async System.Threading.Tasks.Task<List<Cliente>> ListarClientes()
         {
-            throw new NotImplementedException();
+            try
+            {
+                //http://localhost:8080/
+
+                var httpClient = new HttpClient();
+                var json = await httpClient.GetStringAsync("http://localhost:8080/api/Palets/Clientes");
+
+                List<Cliente> clientes = null;
+
+                clientes = JsonConvert.DeserializeObject<List<Cliente>>(json);
+
+                return clientes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Se produjo un error al intentar listar los clientes.");
+            }
         }
 
         private void SetupEvents()
@@ -78,16 +95,66 @@ namespace EmpleadosApp.Droid
 
         private void lvClientesItemSelected_ItemClick(object sender, AdapterView.ItemSelectedEventArgs e)
         {
-            throw new NotImplementedException();
+            try
+            {
+                rutCliente = Convert.ToInt32(e.Id);
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, "ERROR: " + ex.Message, ToastLength.Long).Show();
+            }
         }
 
         private void btnAltaPallet_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            Palet paletAgregar = new Palet();
+
+            try
+            {
+                string producto = etProducto.Text.ToString();
+                decimal peso = Convert.ToDecimal(etPeso.Text);
+                int cantidad = Convert.ToInt32(etCantidad.Text);
+
+                paletAgregar.Cantidad = cantidad;
+                paletAgregar.Peso = peso;
+                paletAgregar.Producto = producto;
+                paletAgregar.Casilla = idRack;
+                paletAgregar.Cliente = rutCliente;
+
+                try
+                {
+
+                    HttpClient client = new HttpClient();
+
+                    string url = "http://localhost:8080/api/Palets/Palet";
+
+                    var content = new StringContent(JsonConvert.SerializeObject(paletAgregar), Encoding.UTF8, "application/json");
+
+                    var result = client.PostAsync(url, content).Result;
+
+                    if (result.Content.ToString().ToUpper() == "TRUE")
+                    {
+                        Toast.MakeText(this, "Se dio de alta con exito el pallet.", ToastLength.Long).Show();
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Error al dar de alta el palet.", ToastLength.Long).Show();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("ERROR!: " + ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, "ERROR: " + ex.Message, ToastLength.Long).Show();
+            }
         }
 
         private void btnAltaCliente_Click(object sender, EventArgs e)
         {
+            //corresponde a otro caso de uso
             throw new NotImplementedException();
         }
 
@@ -108,32 +175,6 @@ namespace EmpleadosApp.Droid
             etPeso = FindViewById<EditText>(Resource.Id.etPeso);
             etProducto = FindViewById<EditText>(Resource.Id.etProducto);
         }
-
-        /*public async System.Threading.Tasks.Task<List<Rack>> ObtenerRacks()
-        {
-            try
-            {
-                //http://localhost:8080/
-
-                var httpClient = new HttpClient();
-                var json = await httpClient.GetStringAsync("http://localhost:8080/api/Palets/Galpon?id=1");
-
-                Galpon galpon = null;
-
-                galpon = JsonConvert.DeserializeObject<Galpon>(json);
-
-                var sector = galpon.Sectores.Where(s => s.Codigo == idSector).Select(c => new {
-                    Sector = c
-                }).FirstOrDefault();
-
-                racks = sector.Sector.Racks;
-
-                return racks;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Se produjo un error al intentar listar los racks.");
-            }
-        }*/
+        
     }
 }
