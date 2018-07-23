b45838059f932a48086a3c1097ab5f15962676e7
Gero Amarillo <geronimoamarillo29@gmail.com>
2018-07-23 15:32:14 -0300

    Ajustes de validacion

--- EntidadesCompartidasCore/Local.cs
+++ EntidadesCompartidasCore/Local.cs
@@ -2,16 +2,20 @@ namespace EntidadesCompartidasCore
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public partial class Local
     {
-
+        [Required]
         public int Id { get; set; }
-        
+        [Required]
+        [MinLength(1)]
+        [MaxLength(150)]
         public string Nombre { get; set; }
-        
+        [MinLength(5)]
+        [MaxLength(250)]
         public string Direccion { get; set; }
-        
         public  List<Entrega> Entregas { get; set; }
         
         public  List<Entrega> Entregas1 { get; set; }
--- EntidadesCompartidasCore/Palet.cs
+++ EntidadesCompartidasCore/Palet.cs
@@ -2,15 +2,24 @@ namespace EntidadesCompartidasCore
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public partial class Palet
     {
+        [Required]
         public int Id { get; set; }
-        
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
         public string Producto { get; set; }
 
+        [Required]
+        [Range(1, 20000)]
         public int Cantidad { get; set; }
-
+        [Required]
+        [Range(1, 1500)]
         public decimal Peso { get; set; }
 
         public int Casilla { get; set; }
