cd0db154c79b15d25c57c68f9ece1491342324df
Pablo <pablorcarmona@hotmail.com>
2018-09-05 18:08:21 -0300

    arreglos en controladores y vistas

--- AdministradoresApp/Controllers/VehiculosController.cs
+++ AdministradoresApp/Controllers/VehiculosController.cs
@@ -404,11 +404,42 @@ namespace AdministradoresApp.Controllers
             }
         }
 
-        public ActionResult Modificar(Vehiculo vehiculo)
+        public ActionResult Modificar(string matricula)
         {
             if (ComprobarLogin() == "G")
             {
-                return View(vehiculo);
+                List<Vehiculo> vehiculos = HttpContext.Session.Get<List<Vehiculo>>(SESSSION_VEHICULOS);
+                Vehiculo vehiculo = vehiculos.FirstOrDefault(x => x.Matricula == matricula);
+                DTVehiculo vehiculoaEditar = new DTVehiculo()
+                {
+                    Matricula = vehiculo.Matricula,
+                    Marca = vehiculo.Marca,
+                    Modelo = vehiculo.Modelo,
+                    Capacidad = vehiculo.Capacidad,
+                    Estado = vehiculo.Estado,
+                    Cadete = vehiculo.Cadete
+                };
+                if(vehiculo is Moto)
+                {
+                    vehiculoaEditar.Cilindrada = ((Moto)vehiculo).Cilindrada;
+                    vehiculoaEditar.Tipo = "Moto";
+                }
+                if(vehiculo is Camioneta)
+                {
+                    vehiculoaEditar.Cabina = ((Camioneta)vehiculo).Cabina;
+                    vehiculoaEditar.Tipo = "Camioneta";
+                }
+                if(vehiculo is Automobil)
+                {
+                    vehiculoaEditar.Puertas = ((Automobil)vehiculo).Puertas;
+                    vehiculoaEditar.Tipo = "Automobil";
+                }
+                if(vehiculo is Camion)
+                {
+                    vehiculoaEditar.Altura = ((Camion)vehiculo).Altura;
+                    vehiculoaEditar.Tipo = "Camion";
+                }
+                return View(vehiculoaEditar);
             }
             else
             {
@@ -419,7 +450,7 @@ namespace AdministradoresApp.Controllers
         }
 
         [HttpPost]
-        public ActionResult ModificarPost([FromForm]Vehiculo unVehiculo)
+        public ActionResult ModificarCamioneta([FromForm]DTVehiculo unVehiculo)
         {
             try
             {
@@ -430,7 +461,18 @@ namespace AdministradoresApp.Controllers
 
                     if (ModelState.IsValid)
                     {
-                        bool exito = controladorVehiculo.ModificarVehiculo(unVehiculo);
+                        Camioneta vehiculoaModificar = new Camioneta()
+                        {
+                            Matricula = unVehiculo.Matricula,
+                            Marca = unVehiculo.Marca,
+                            Modelo = unVehiculo.Modelo,
+                            Capacidad = unVehiculo.Capacidad,
+                            Estado = unVehiculo.Estado,
+                            Cadete = unVehiculo.Cadete,
+                            Cabina = unVehiculo.Cabina
+                        };
+
+                        bool exito = controladorVehiculo.ModificarVehiculo(vehiculoaModificar);
 
                         if (exito)
                         {
@@ -463,7 +505,175 @@ namespace AdministradoresApp.Controllers
             }
         }
 
-            public ActionResult ListarAutos()
+        [HttpPost]
+        public ActionResult ModificarCamion([FromForm]DTVehiculo unVehiculo)
+        {
+            try
+            {
+                string mensaje = "";
+                if (ComprobarLogin() == "G")
+                {
+                    IControladorVehiculo controladorVehiculo = FabricaApps.GetControladorVehiculo();
+
+                    if (ModelState.IsValid)
+                    {
+                        Camion vehiculoaModificar = new Camion()
+                        {
+                            Matricula = unVehiculo.Matricula,
+                            Marca = unVehiculo.Marca,
+                            Modelo = unVehiculo.Modelo,
+                            Capacidad = unVehiculo.Capacidad,
+                            Estado = unVehiculo.Estado,
+                            Cadete = unVehiculo.Cadete,
+                            Altura = unVehiculo.Altura
+                        };
+
+                        bool exito = controladorVehiculo.ModificarVehiculo(vehiculoaModificar);
+
+                        if (exito)
+                        {
+                            mensaje = "El vehiculo se modificó con exito!.";
+                        }
+                        else
+                        {
+                            mensaje = "Se produjo un error al modificar el vehiculo!.";
+                        }
+                    }
+
+
+                    if (mensaje != "")
+                    {
+                        HttpContext.Session.Set<string>(SESSION_MENSAJE, mensaje);
+                    }
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    HttpContext.Session.Set<string>(SESSION_MENSAJE, "No hay un usuario de tipo Administrador General logueado en el sistema");
+
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ModificarMoto([FromForm]DTVehiculo unVehiculo)
+        {
+            try
+            {
+                string mensaje = "";
+                if (ComprobarLogin() == "G")
+                {
+                    IControladorVehiculo controladorVehiculo = FabricaApps.GetControladorVehiculo();
+
+                    if (ModelState.IsValid)
+                    {
+                        Moto vehiculoaModificar = new Moto()
+                        {
+                            Matricula = unVehiculo.Matricula,
+                            Marca = unVehiculo.Marca,
+                            Modelo = unVehiculo.Modelo,
+                            Capacidad = unVehiculo.Capacidad,
+                            Estado = unVehiculo.Estado,
+                            Cadete = unVehiculo.Cadete,
+                            Cilindrada = unVehiculo.Cilindrada
+                        };
+
+                        bool exito = controladorVehiculo.ModificarVehiculo(vehiculoaModificar);
+
+                        if (exito)
+                        {
+                            mensaje = "El vehiculo se modificó con exito!.";
+                        }
+                        else
+                        {
+                            mensaje = "Se produjo un error al modificar el vehiculo!.";
+                        }
+                    }
+
+
+                    if (mensaje != "")
+                    {
+                        HttpContext.Session.Set<string>(SESSION_MENSAJE, mensaje);
+                    }
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    HttpContext.Session.Set<string>(SESSION_MENSAJE, "No hay un usuario de tipo Administrador General logueado en el sistema");
+
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ModificarAutomobil([FromForm]DTVehiculo unVehiculo)
+        {
+            try
+            {
+                string mensaje = "";
+                if (ComprobarLogin() == "G")
+                {
+                    IControladorVehiculo controladorVehiculo = FabricaApps.GetControladorVehiculo();
+
+                    if (ModelState.IsValid)
+                    {
+                        Automobil vehiculoaModificar = new Automobil()
+                        {
+                            Matricula = unVehiculo.Matricula,
+                            Marca = unVehiculo.Marca,
+                            Modelo = unVehiculo.Modelo,
+                            Capacidad = unVehiculo.Capacidad,
+                            Estado = unVehiculo.Estado,
+                            Cadete = unVehiculo.Cadete,
+                            Puertas = unVehiculo.Puertas
+                        };
+
+                        bool exito = controladorVehiculo.ModificarVehiculo(vehiculoaModificar);
+
+                        if (exito)
+                        {
+                            mensaje = "El vehiculo se modificó con exito!.";
+                        }
+                        else
+                        {
+                            mensaje = "Se produjo un error al modificar el vehiculo!.";
+                        }
+                    }
+
+
+                    if (mensaje != "")
+                    {
+                        HttpContext.Session.Set<string>(SESSION_MENSAJE, mensaje);
+                    }
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    HttpContext.Session.Set<string>(SESSION_MENSAJE, "No hay un usuario de tipo Administrador General logueado en el sistema");
+
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
+
+        public ActionResult ListarAutos()
         {
             if (ComprobarLogin() == "G")
             {
