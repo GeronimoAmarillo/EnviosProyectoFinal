0cafa9c27bd31a0dc9a421f8f97ee88ac3f84585
Gero Amarillo <geronimoamarillo29@gmail.com>
2018-10-08 16:24:25 -0300

    Caso de Uso terminado y Testeado

--- ClientesApp/Controllers/PaquetesController.cs
+++ ClientesApp/Controllers/PaquetesController.cs
@@ -58,6 +58,13 @@ namespace ClientesApp.Controllers
 
                     ViewBag.Locales = localesDDL;
 
+                    List<object> estados = new List<object>();
+                    estados.Add(new {Value = "Levantado", Text = "Levantado" });
+                    estados.Add(new { Value = "Entregado", Text = "Entregado" });
+                    estados.Add(new { Value = "Extraviado", Text = "Extraviado" });
+
+                    ViewBag.Estados = estados;
+
                     if (filtrados != null)
                     {
                         HttpContext.Session.Set<List<Paquete>>(SESSION_FILTRADOS, null);
@@ -89,39 +96,85 @@ namespace ClientesApp.Controllers
         
         public ActionResult Filtrar(string estado, DateTime fechaSalida, int? local, string criterio)
         {
-            if (ComprobarLogin())
+            try
             {
-                List<Paquete> paquetes = HttpContext.Session.Get<List<Paquete>>(SESSION_PAQUETES);
-
-                List<Paquete> filtradosResultado = new List<Paquete>();
-
-                if (criterio == "Estado")
+                if (ComprobarLogin())
                 {
-                    filtradosResultado = paquetes.Where(x => x.Estado.ToUpper() == estado).ToList();
-                }
+                    List<Paquete> paquetes = HttpContext.Session.Get<List<Paquete>>(SESSION_PAQUETES);
 
-                if (criterio == "Fecha")
+                    List<Paquete> filtradosResultado = new List<Paquete>();
+
+                    if (criterio == "Estado")
+                    {
+                        filtradosResultado = paquetes.Where(x => x.Estado.ToUpper() == estado.ToUpper()).ToList();
+                    }
+
+                    if (criterio == "Fecha")
+                    {
+                        filtradosResultado = paquetes.Where(x => x.FechaSalida.ToShortDateString() == fechaSalida.ToShortDateString()).ToList();
+                    }
+
+                    if (criterio == "Local")
+                    {
+                        filtradosResultado = paquetes.Where(x => x.Entregas.LocalReceptor == local).ToList();
+                    }
+
+                    if (criterio == "Todos")
+                    {
+                        filtradosResultado = paquetes.Where(x => x.Estado.ToUpper() == estado.ToUpper() && x.FechaSalida.ToShortDateString() == fechaSalida.ToShortDateString() && x.Entregas.LocalReceptor == local).ToList();
+                    }
+
+                    HttpContext.Session.Set<List<Paquete>>(SESSION_FILTRADOS, filtradosResultado);
+
+                    return RedirectToAction("Index", "Paquetes", new { area = "" });
+                }
+                else
                 {
-                    filtradosResultado = paquetes.Where(x => x.FechaSalida == fechaSalida).ToList();
+                    HttpContext.Session.Set<string>(SESSION_MENSAJE, "No hay un Cliente logueado en el sistema");
+
+                    return RedirectToAction("Index", "Home", new { area = "" });
                 }
-
-                if (criterio == "Local")
-                {
-                    filtradosResultado = paquetes.Where(x => x.Entregas.LocalReceptor == local).ToList();
-                }
-
-                if (criterio == "Todos")
-                {
-                    filtradosResultado = paquetes.Where(x => x.Estado.ToUpper() == estado && x.FechaSalida == fechaSalida && x.Entregas.LocalReceptor == local).ToList();
-                }
-
-                HttpContext.Session.Set<List<Paquete>>(SESSION_FILTRADOS, filtradosResultado);
-
-                return RedirectToAction("Index", "Paquetes", new { area = "" });
             }
-            else
+            catch (Exception ex)
             {
-                HttpContext.Session.Set<string>(SESSION_MENSAJE, "No hay un Cliente logueado en el sistema");
+                HttpContext.Session.Set<string>(SESSION_MENSAJE, "Error al mostrar el formulario: No se pudieron listar los Paquetes filtrados");
+
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+           
+        }
+
+        public async Task<ActionResult> Detalles(int numReferencia)
+        {
+            try
+            {
+                if (ComprobarLogin())
+                {
+                    IControladorConsultasPaquete controladorPaquete = FabricaApps.GetControladorConsultasPaquete();
+
+                    Paquete paquete = await controladorPaquete.BuscarPaquete(numReferencia);
+
+                    if (paquete != null)
+                    {
+                        return View(paquete);
+                    }
+                    else
+                    {
+                        HttpContext.Session.Set<string>(SESSION_MENSAJE, "No existe el paquete seleccionado");
+
+                        return RedirectToAction("Index", "Paquetes", new { area = "" });
+                    }
+                }
+                else
+                {
+                    HttpContext.Session.Set<string>(SESSION_MENSAJE, "No hay un Cliente logueado en el sistema");
+
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Session.Set<string>(SESSION_MENSAJE, "Error al mostrar el formulario: No se pudo cargar el Paquete");
 
                 return RedirectToAction("Index", "Home", new { area = "" });
             }
