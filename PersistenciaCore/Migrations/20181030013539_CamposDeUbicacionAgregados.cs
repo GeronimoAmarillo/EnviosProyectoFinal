cefa49be11ecf3241d54b1ae4725fec3eb93ec45
Gero Amarillo <geronimoamarillo29@gmail.com>
2018-10-29 22:39:43 -0300

    Agregados Longitud y Latitud al Cadete

---
+++ PersistenciaCore/Migrations/20181030013539_CamposDeUbicacionAgregados.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PersistenciaCore.Migrations
+{
+    public partial class CamposDeUbicacionAgregados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropColumn(
+                name: "IdTelefono",
+                table: "Cadetes");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RUT",
+                table: "Ingresos",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Latitud",
+                table: "Cadetes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Longitud",
+                table: "Cadetes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropColumn(
+                name: "Latitud",
+                table: "Cadetes");
+
+            migrationBuilder.DropColumn(
+                name: "Longitud",
+                table: "Cadetes");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RUT",
+                table: "Ingresos",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "IdTelefono",
+                table: "Cadetes",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
