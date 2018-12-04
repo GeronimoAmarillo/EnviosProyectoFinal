5dc3bf2ad64879f0a7a6f0d11b92111712f4c246
Matias Lopez <enviosmatiaslopez@gmail.com>
2018-12-04 00:03:24 -0300

    prueba

---
+++ ClientesApp/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntidadesCompartidasCore;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ClientesApp.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var usuarioLogueado = HttpContext.Session.Get<Usuario>("UsuarioLogueado");
+            return View(usuarioLogueado);
+        }
+    }
+}
